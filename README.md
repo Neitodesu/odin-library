42b90015c63636bfa301d1bffa35843cecf269e4
Neitodesu <95891209+Neitodesu@users.noreply.github.com>
2026-01-16 23:06:50 -0500

    started layout/fuctionality

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# odin-library
+# Odin Library
+
+1. All books will be stored in an array
+2. Use constructor to create new books
+3. Use a function (outside of constructor) that will take arguments and create a book object, then store that book object into the library array
+   Each book should be created with a unique ID using **`crypto.randomUUID()`**
+
+## Next Steps
+
+1. Use a function that loops through the array and displays each book to the user. Use a "card" display
+2. Add a button to make a modal pop up with the form to allow user to submit info(create book object):
+   - Author
+   - Title
+   - of pages
+   - If read
+3. Add a button on each "card" to remove book from library.
+4. Look into data-attributes for the corresponding unique ID
+5. Add a button to each "card" to change its 'read status'
---
+++ js/app.js
@@ -0,0 +1,68 @@
+let library = [];
+
+const addBookButton = document.querySelector('#addBookButton');
+const authorName = document.querySelector('#author');
+const bookTitle = document.querySelector('#title');
+const bookPages = document.querySelector('#pages');
+const hasReadInput = document.querySelector('#hasRead');
+
+let isValid = false;
+
+class Book {
+  constructor(author, title, pages, hasRead, id) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.id = id;
+  }
+}
+
+const createBook = (author, title, pages, hasRead) => {
+  const bookId = crypto.randomUUID();
+  let book = new Book(author, title, pages, hasRead, bookId);
+
+  return book;
+};
+
+const updateLibrary = (author, title, pages, hasRead) => {
+  let newBook = createBook(author, title, pages, hasRead);
+
+  library.push(newBook);
+};
+
+const resetForm = () => {
+  authorName.value = '';
+  bookTitle.value = '';
+  bookPages.value = '';
+  isValid = false;
+};
+
+const validateForm = () => {
+  if (authorName.value == '') {
+    return;
+  }
+  if (bookTitle.value == '') {
+    return;
+  }
+  if (bookPages.value == '') {
+    return;
+  }
+  if (bookPages.value != +bookPages.value) {
+    return;
+  }
+
+  isValid = true;
+};
+addBookButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const checkRead = true ? hasReadInput.checked : false;
+
+  validateForm();
+  if (!isValid) {
+    return;
+  }
+  updateLibrary(authorName.value, bookTitle.value, +bookPages.value, checkRead);
+  resetForm();
+  console.log(library);
+});
